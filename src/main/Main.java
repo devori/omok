c36392e10a5b716d9a7b34abddd941e3cd33652c
devori.lee <devori.lee@gmail.com>
2016-03-31 21:03:47 +0900

    add package. change gitignore

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @EnableAutoConfiguration
 public class Main {
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
