12dd16b4557f174cf035a8c9eddcfed84364fb65
Jade Chang and Scott Im <pair+jumpitup82+itanoss@gmail.com>
2016-04-05 15:07:47 +0900

    스프링부트 반영함

--- src/main/java/omok/Main.java
+++ src/main/java/omok/Main.java
@@ -1,6 +1,10 @@
+package omok;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 @EnableAutoConfiguration
 public class Main {
     public static void main(String[] args) {
